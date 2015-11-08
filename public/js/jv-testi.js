d2e0012b7ebbda6a0382a7f0e2467438b96519fe
Juha Viljanen <juha.wiljanen@gmail.com>
2015-11-08 13:27:12 +0200

    Session storage read added

--- public/js/jv-testi.js
+++ public/js/jv-testi.js
@@ -1,5 +1,13 @@
 
     $(function() {
+        var uName;
+        if (window.sessionStorage.getItem('RTFtoken')&&window.sessionStorage.getItem('RTFtoken').length>0){
+            userName=window.sessionStorage.getItem('RTFuser');
+        }else{
+            uName='No login';
+        }
+        console.log('username: '+uName);
+
         $( "#tabsiX" ).tabs();
     });
 
